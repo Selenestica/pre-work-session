7cea2aaae46aa4b12d83a398e45b266f29c5f0e1
JB Wilson <53917177+Selenestica@users.noreply.github.com>
2020-08-08 10:06:34 -0500

    helped rob with css and js

---
+++ scripty.js
@@ -0,0 +1,5 @@
+var button = document.getElementById("myButton");
+var boxB = document.getElementsByClassName("box");
+button.addEventListener("click", function () {
+  boxB.style = "background-color: blue;";
+});
